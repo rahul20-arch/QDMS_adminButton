3ba08e7fbca2b2f1e5ba29d7bf4039a9b836f22c
rahul20-arch <sivapalanrahul@gmail.com>
2023-01-27 15:43:52 +0530

    last

--- src/test/java/base/base.java
+++ src/test/java/base/base.java
@@ -1,17 +1,26 @@
 package base;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-public class base {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
+public class base {
+	public static ExtentReports extent;
+    public static ExtentHtmlReporter htmlreporter;
+    public static ExtentTest testCase;
 
 	public static WebDriver driver;
 	
 	
 	@BeforeSuite
-	public void main() {
+	public void main() throws IOException {
 		
 		System.setProperty("WebDriver.chrome.driver","C:\\Program Files\\chromedriver_selenium/chromedriver.exe");
 		 driver = new ChromeDriver();
@@ -19,8 +28,18 @@ public class base {
 		 driver.manage().window().maximize();
 		 driver.get("http://192.168.1.35:8083/#/login");
 		 
-		 //http://qdms-phase-3.s3-website.ap-south-1.amazonaws.com/
 		
+		 //http://qdms-phase-3.s3-website.ap-south-1.amazonaws.com/
+		extentreport();
+	}
+	@AfterSuite
+	public void close() {
+		extent.flush();
+	}
+	public static void extentreport() throws IOException {
+		extent =new ExtentReports();
+		htmlreporter = new ExtentHtmlReporter("C:\\Users\\hp\\eclipse-workspace\\QDMS_Auto\\report\\report.html");
+		extent.attachReporter(htmlreporter);
 	}
 
 }
--- src/test/java/testcase/adminButtontest.java
+++ src/test/java/testcase/adminButtontest.java
@@ -1,135 +1,319 @@
 package testcase;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.support.PageFactory;
-
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import com.aventstack.extentreports.Status;
+
 import base.base;
 import pages.adminButton;
+//import io.qameta.allure.Step;
 
 public class adminButtontest extends base {
+
 	adminButton adminbtn = new adminButton();
+
 	@Test
-public void adminButton() throws InterruptedException, IOException{
-	
-		
-		
-		
-		
-		
-		
-	PageFactory.initElements(driver,adminbtn);
-	
-	adminButton.username.sendKeys("admin");
-	adminButton.password.sendKeys("tokyo@admin");
-	adminButton.login.click();
-	
-	Thread.sleep(2000);
-	//adminButton.CheckAdmin.click();
+	public void adminButton() throws InterruptedException, IOException {
 
-	
-	
-	SoftAssert aa = new SoftAssert();
-	Point actual = adminButton.CheckAdmin.getLocation();
-	
-	int x = actual.getX();
-	int expectedresult = 1244;
-	System.out.println("x-axis = " + x);
-	aa.assertEquals(x, expectedresult);
-	
-	
-	int y = actual.getY();
-	int expectresult =  18;
-	System.out.println("y-axis = " + y);
-	aa.assertEquals(expectresult, y);
-	
-	
-	org.openqa.selenium.Dimension size = adminButton.CheckAdmin.getSize();
-	int width = size.getWidth();
-	System.out.println("width  = " + width);
-	int expectedresult1 =84;
-	aa.assertEquals(width, expectedresult1);	
-	 
-	int height = size.getHeight();
-	System.out.println("height = " + height);
-	int expectedresult2 =43;
-	aa.assertEquals(height, expectedresult2);
-	
-	
-	String backgroundColor = adminButton.CheckAdmin.getCssValue("background-color");
-	System.out.println("backgroundclr = " + backgroundColor );
-	String bgc = "rgba(0, 0, 0, 0)";
-	aa.assertEquals(backgroundColor, bgc);
-	
-	
-	
-	
-	String borderColor = adminButton.CheckAdmin.getCssValue("border-color");
-	System.out.println("borderColor = " + borderColor);
-	String bc = "rgb(255, 255, 255)";
-	aa.assertEquals(borderColor, bc);
-	
+		PageFactory.initElements(driver, adminbtn);
 
-	
-	
-	
+		adminButton.username.sendKeys("admin");
+		adminButton.password.sendKeys("tokyo@admin");
+		adminButton.login.click();
 
-	String font = adminButton.CheckAdmin.getCssValue("font-family");
-    System.out.println("Font of button: " + font);
-	String ft = "Roboto, sans-serif";
-	aa.assertEquals(font, ft);
-	
-	
-	
-    String color = adminButton.CheckAdmin.getCssValue("color");
-	System.out.println("Font color of button: " + color);
-	String fontclr = "rgba(255, 255, 255, 1)";
-	aa.assertEquals(fontclr, color);
-	
-	
-	 String txt = adminButton.CheckAdmin.getText();
-	 System.out.println("text = " + txt);
-	 String txxt = "admin";
-	 aa.assertEquals(txt, txxt);
-	 
-	 aa.assertAll();
-	
-//	SpellChecker spellChecker = new SpellChecker();
-//	spellChecker.addDictionary(new SpellDictionaryHashMap());
-//	List<String> mispelledWords = spellChecker.getMispelledWords(text);
-//	String text = adminButton.CheckAdmin.getText();
-//	System.out.println("Text of button: " + text);
-//	String txt = text;
+		Thread.sleep(2000);
+		// adminButton.CheckAdmin.click();
+
+//	Boolean true1;
+//	Boolean False1;
 //	
-	
+
+		//SoftAssert aa = new SoftAssert();
+		boolean AcctualElement = adminButton.CheckAdmin.isDisplayed();
+		adminButton.CheckAdmin.isDisplayed();
+		boolean ExpectedElement = true;
+		//AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+
+		boolean elementvisible = true;
+		testCase = extent.createTest("ELEMENT-VISIBLE");
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			elementvisible = false;
+		}
+		if (elementvisible) {
+			testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+			testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+			testCase.log(Status.INFO, "Element Available");
+			testCase.log(Status.PASS, "Correct element");
+		} else {
+			testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+			testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+			testCase.log(Status.INFO, "Element not available");
+			testCase.log(Status.FAIL, "No Element");
+		}
+		Thread.sleep(2000);
+
+		Point actual = adminButton.CheckAdmin.getLocation();
+
+		int x = actual.getX();
+		int y = actual.getY();
+		//int expectresult = 18;
+		//int expectedresult = 1244;
+		System.out.println("x-axis = " + x);
+		System.out.println("y-axis = " + y);
+		
+		//aa.assertEquals(x, expectedresult);
+		Point expectedresult = new Point(1244, 18);
+		testCase = extent.createTest("position");
+		boolean position = true;
+		
+		try {
+			AssertJUnit.assertEquals(actual, expectedresult);
+		} catch (AssertionError e) {
+			position = false;
+		}
+			if (position) {
+				testCase.log(Status.INFO, "ActualElement :- " + actual);
+				testCase.log(Status.INFO, "ExpectedElement :- " + expectedresult);
+				testCase.log(Status.INFO, "correct postion");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + actual);
+				testCase.log(Status.INFO, "ExpectedElement :- " + expectedresult);
+				testCase.log(Status.INFO, "wrong postion");
+				testCase.log(Status.FAIL, "wrong");
+			}
+			Thread.sleep(2000);
+		
+		
+
+		
+		
+		//AssertJUnit.assertEquals(expectresult, y);
+
+//		boolean y_axis = true;
+//		testCase = extent.createTest("x_axis");
+//		try {
+//			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+//		} catch (AssertionError e) {
+//			y_axis = false;
+//
+//			if (y_axis) {
+//				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+//				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+//				testCase.log(Status.INFO, "correct y_axis");
+//				testCase.log(Status.PASS, "Correct ");
+//			} else {
+//				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+//				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+//				testCase.log(Status.INFO, "wrong y_axis");
+//				testCase.log(Status.FAIL, "wrong");
+//			}
+//
+//	}
+
+		Thread.sleep(2000);
+
+		org.openqa.selenium.Dimension size = adminButton.CheckAdmin.getSize();
+		int width = size.getWidth();
+		System.out.println("width  = " + width);
+		int expectedresult1 = 84;
+		//AssertJUnit.assertEquals(width, expectedresult1);
+		testCase = extent.createTest("width");
+		boolean width1 = true;
+		
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			width1 = false;
+		}
+		if (width1) {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "correct width");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "wrong width");
+				testCase.log(Status.FAIL, "wrong");
+			}
+
+		
+
+		Thread.sleep(2000);
+
+		int height = size.getHeight();
+		System.out.println("height = " + height);
+		int expectedresult2 = 43;
+		//AssertJUnit.assertEquals(height, expectedresult2);
+
+		boolean height1 = true;
+		testCase = extent.createTest("height");
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			height1 = false;
+
+		}
+		if (height1) {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "correct height");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "wrong height");
+				testCase.log(Status.FAIL, "wrong");
+			}
+
+		
+
+		Thread.sleep(2000);
+
+		String backgroundColor = adminButton.CheckAdmin.getCssValue("background-color");
+		System.out.println("backgroundclr = " + backgroundColor);
+		String bgc = "rgba(0, 0, 0, 0)";
+		//AssertJUnit.assertEquals(backgroundColor, bgc);
+		boolean backgroundColor1 = true;
+		testCase = extent.createTest("backgroundColor");
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			backgroundColor1 = false;
+
+		}
+			if (backgroundColor1) {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "correct backgroundColor");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "wrong backgroundColor");
+				testCase.log(Status.FAIL, "wrong");
+			}
+
+		
+		Thread.sleep(2000);
+
+		String borderColor = adminButton.CheckAdmin.getCssValue("border-color");
+		System.out.println("borderColor = " + borderColor);
+		String bc = "rgb(255, 255, 255)";
+		//AssertJUnit.assertEquals(borderColor, bc);
+
+		boolean borderColor1 = true;
+		testCase = extent.createTest("borderColor");
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			borderColor1 = false;
+		}
+			if (borderColor1) {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "correct borderColor");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "wrong borderColor");
+				testCase.log(Status.FAIL, "wrong");
+			}
+
 	
 
-//	boolean rahul = x == 1244 && y == 18 && width == 84 && height == 43 && backgroundColor == bgc &&  borderColor == bc && font == ft && text == txt;
-//	if ( rahul ) {
-//		System.out.println("pass");
-//		
-//	}else 
-//		System.out.println("fail");
-//	
+		Thread.sleep(2000);
+
+		String font = adminButton.CheckAdmin.getCssValue("font-family");
+		System.out.println("Font of button: " + font);
+		String ft = "Roboto, sans-serif";
+		//AssertJUnit.assertEquals(font, ft);
+
+		boolean font1 = true;
+		testCase = extent.createTest("font");
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			font1 = false;
+		}
+			if (font1) {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "correct font");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "wrong font");
+				testCase.log(Status.FAIL, "wrong");
+			}
+
 		
-	//assertEquals(false, false);
-	
-	
-}
-	
+
+		Thread.sleep(2000);
+		String color = adminButton.CheckAdmin.getCssValue("color");
+		System.out.println("Font color of button: " + color);
+		String fontclr = "rgba(255, 255, 255, 1)";
+		//AssertJUnit.assertEquals(fontclr, color);
+
+		boolean color1 = true;
+		testCase = extent.createTest("color");
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			color1 = false;
+		}
+			if (color1) {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "correct color");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "wrong color");
+				testCase.log(Status.FAIL, "wrong");
+			}
+
 		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+		Thread.sleep(2000);
+
+		String txt = adminButton.CheckAdmin.getText();
+		System.out.println("text = " + txt);
+		String txxt = "admin";
+		AssertJUnit.assertEquals(txt, txxt);
+
+		boolean txt1 = true;
+		testCase = extent.createTest("color");
+		try {
+			AssertJUnit.assertEquals(AcctualElement, ExpectedElement);
+		} catch (AssertionError e) {
+			txt1 = false;
+		}
+			if (txt1) {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "correct text");
+				testCase.log(Status.PASS, "Correct ");
+			} else {
+				testCase.log(Status.INFO, "ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO, "ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO, "wrong text");
+				testCase.log(Status.FAIL, "wrong");
+			}
+
+		
+
+	}
+	}
+
